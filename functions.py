ff7b4589fed995c444916fab27bd705353140c8d
Richard Riley <48334712+reriley@users.noreply.github.com>
2023-11-23 17:39:20 +0100

    Working version without language dictionary check.

--- functions.py
+++ functions.py
@@ -4,32 +4,38 @@ import cmudict
 PRONOUNACABLE_WORDS = cmudict.dict().keys()
 
 
-def decompose_name(name: str) -> Counter:
-    part_counter = Counter()
-    words = name.split(' ')
-    for w in words:
-        part_counter.update(decompose_word(w))
-    return part_counter
+class Decomposer:
 
+    def __init__(self):
+        self.language_dictionary_api_queries = 0
 
-def decompose_word(word: str) -> Counter:
-    word = word.lower() + '_'  # Append a meaningless character to facilitate counting backwards
-    word_length = len(word)
-    part_counter = Counter()
-    unevaluated = Counter(range(word_length))
-    for i in range(word_length):
-        if i not in unevaluated:
-            pass
-        else:
-            for j in range(word_length-i):
-                nugget = word[i:-j-1]  # Count backwards
-                cond1 = len(nugget) > 1
-                cond2 = nugget.lower() in PRONOUNACABLE_WORDS
-                cond3 = True  # Placeholder: Is the string in the dictionary?
+    def decompose_name(self, name: str) -> Counter:
+        name_part_counter = Counter()
+        words = name.split(' ')
+        for w in words:
+            name_part_counter.update(self.decompose_word(w))
+        return name_part_counter
 
-                if cond1 and (cond2 or cond3):
-                    part_counter.update([nugget])
-                    unevaluated.subtract(range(i, word_length-j-1))
-                    unevaluated._keep_positive()
-                    break
-    return part_counter
+    def decompose_word(self, word: str) -> Counter:
+        word = word.lower() + '_'  # Append a meaningless character to facilitate counting backwards
+        word_length = len(word)
+        word_part_counter = Counter()
+        unevaluated = Counter(range(word_length))
+        for i in range(word_length):
+            if i not in unevaluated:
+                pass
+            else:
+                for j in range(word_length-i):
+                    nugget = word[i:-j-1]  # Count backwards
+                    cond1 = len(nugget) > 1
+                    cond2 = nugget.lower() in PRONOUNACABLE_WORDS
+                    cond3 = False
+                    if cond1 and not cond2:
+                        cond3 = False  # Placeholder: Is the string in the dictionary?
+                        self.language_dictionary_api_queries += 1
+                    if cond1 and (cond2 or cond3):
+                        word_part_counter.update([nugget])
+                        unevaluated.subtract(range(i, word_length-j-1))
+                        unevaluated._keep_positive()
+                        break
+        return word_part_counter
